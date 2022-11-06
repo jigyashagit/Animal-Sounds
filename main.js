ea87850cea5b24bbdacb1f4dbc5befc772392813
jigyashagit <109466868+jigyashagit@users.noreply.github.com>
2022-11-06 17:42:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function startclassification()
+{
+  navigator.mediaDevices.getUserMedia({audio:true});
+  classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/oRhVL4j87/model.json",modelReady);
+}
+
+function modelReady()
+{
+  classifier.classify(gotResults);
+}
+
+function gotResults(error,results){
+  console.log("got results")
+}
